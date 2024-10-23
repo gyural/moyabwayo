1c716fa85c2de7557ac9c58e96e3f80cde42b8fa
Lim, gyuseong <100032171+gyural@users.noreply.github.com>
2024-10-23 12:38:52 +0900

    fix: GET-party-dates-오름차순 불러오게 설정

--- src/main/java/com/moyeobwayo/moyeobwayo/Domain/dto/PartyResponseDTO.java
+++ src/main/java/com/moyeobwayo/moyeobwayo/Domain/dto/PartyResponseDTO.java
@@ -6,6 +6,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.Comparator;
 import java.util.List;
 
 @AllArgsConstructor @NoArgsConstructor
@@ -17,13 +18,18 @@ public class PartyResponseDTO {
     public void setParty(Party party) {
         this.party = party;
 
+        this.party.getDates().sort(Comparator.comparing(date -> date.getSelected_date()));
         // party의 dates와 timeslots에서 UserEntityDTO 생성
         this.party.getDates().forEach(date -> {
-            date.getTimeslots().forEach(timeslot -> {
-                if(timeslot.getUserEntity() != null){
+            if (date.getTimeslots() != null) {
+                date.getTimeslots().forEach(timeslot -> {
+                    if (timeslot.getUserEntity() != null) {
+                        // 비밀번호 정보 제거
                         timeslot.getUserEntity().setPassword(null);
-                }
-            });
+                    }
+                });
+            }
         });
+        //또 date의 selectedDate가 오름차순 정령되게 하고 싶어
     }
 }
