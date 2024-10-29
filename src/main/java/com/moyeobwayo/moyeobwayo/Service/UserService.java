6a73597a1c8744bdf942447cb5e1f471cb46b76a
fairynayoung <nayoung031220@naver.com>
2024-10-30 03:50:34 +0900

    fix: 카카ã오 파티 로그인 동명이인 이슈 해결

--- src/main/java/com/moyeobwayo/moyeobwayo/Service/UserService.java
+++ src/main/java/com/moyeobwayo/moyeobwayo/Service/UserService.java
@@ -26,8 +26,8 @@ public class UserService {
         this.alarmRepository = alarmRepository;
     }
 
-    // 로그인 로직: 파티 내 중복 이름 확인 및 로그인 처리
-    public Optional<UserResponse> loginOrRegister(String userName, String password, String partyId, boolean isKakao) {
+    // 사용자 로그인 및 등록 처리
+    public Optional<UserResponse> loginOrRegister(String userName, String password, String partyId, boolean isKakao, String kakaoUserId) {
         // 파티 ID로 해당 파티 조회
         Optional<Party> partyOptional = partyRepository.findById(partyId);
         if (partyOptional.isEmpty()) {
@@ -36,13 +36,11 @@ public class UserService {
         Party party = partyOptional.get(); // 파티가 존재하면 파티 객체를 가져옴
 
         // 기존 사용자 확인
-        Optional<UserEntity> existingUser = userRepository.findUserInSameParty(userName, partyId);
+        Optional<UserEntity> existingUser = userRepository.findUserInSameParty(userName + "_" + kakaoUserId, partyId);
         if (existingUser.isPresent()) {
             // 기존 사용자가 있으면 비밀번호 확인 후 로그인 처리
             UserEntity user = existingUser.get();
-            // ✨ 기존 when2meet 로직과 최대한 비슷하게 처리
             if (password == null || password.equals(user.getPassword())) {
-                // [알고리즘]비밀번호가 없거나, 작성시에 같으면 로그인 성공
                 return Optional.of(new UserResponse(user, "기존 회원 로그인이 완료되었습니다."));
             } else {
                 return Optional.empty(); // 비밀번호 불일치로 로그인 실패
@@ -51,7 +49,7 @@ public class UserService {
 
         // 사용자가 없으면 새로 회원가입 (회원가입 기능)
         UserEntity newUser = new UserEntity();
-        newUser.setUserName(userName);
+        newUser.setUserName(userName + "_" + kakaoUserId); // 카카오 유저 ID를 붙임
         newUser.setPassword(password);
         newUser.setParty(party);
 
