e1012b850623e8a7283072a6388a2675d51aacb2
dgsim126 <84228539+dgsim126@users.noreply.github.com>
2024-10-31 22:37:17 +0900

    Token (#55)
    * feat: ë내 현재 상태 불러오기
    
    * fix: Remove unnecessary arguments
    
    * feat: link makes alarm entity
    
    * feat: getInfoAndPhoto
    
    * feat: token added
    
    ---------
    
    Co-authored-by: Lim, gyuseong <dla93190@naver.com>

--- src/main/java/com/moyeobwayo/moyeobwayo/Domain/response/UserResponse.java
+++ src/main/java/com/moyeobwayo/moyeobwayo/Domain/response/UserResponse.java
@@ -9,9 +9,16 @@ import lombok.Setter;
 public class UserResponse {
     private UserEntity user;
     private String message;
+    private String token; // JWT 토큰 추가
 
     public UserResponse(UserEntity user, String message) {
         this.user = user;
         this.message = message;
+    }
+
+    public UserResponse(UserEntity user, String message, String token) {
+        this.user = user;
+        this.message = message;
+        this.token = token;
     }
 }
---
+++ src/main/java/com/moyeobwayo/moyeobwayo/Service/JwtService.java
@@ -0,0 +1,25 @@
+package com.moyeobwayo.moyeobwayo.Service;
+
+import org.springframework.stereotype.Service;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+
+import java.util.Date;
+
+@Service
+public class JwtService {
+    private final String SECRET_KEY = "donggeunisgreatestintheworldshihyunisprettestintheworld";
+
+    public String generateToken(Long kakaoUserId, String nickname, String profileImage) {
+        return Jwts.builder()
+                .claim("kakao_user_id", kakaoUserId)
+                .claim("nickname", nickname)
+                .claim("profile_image", profileImage)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 24)) // 만료 시간 설정
+                .signWith(SignatureAlgorithm.HS256, SECRET_KEY)
+                .compact();
+    }
+}
+
