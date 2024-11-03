9b683f1f608cac45b1e66cc97dd7f7778f9237ec
dgsim <dgsim126@gmail.com>
2024-11-03 20:03:39 +0900

    feat: possible impossible

--- src/main/java/com/moyeobwayo/moyeobwayo/Domain/dto/TimeSlot.java
+++ src/main/java/com/moyeobwayo/moyeobwayo/Domain/dto/TimeSlot.java
@@ -10,7 +10,8 @@ import java.time.LocalDateTime;
 @Getter @Setter
 @AllArgsConstructor @NoArgsConstructor
 public class TimeSlot {
-    String user;
+    private Long userId;            // 사용자 ID 필드 추가
+    private String userName;         // 사용자 이름 필드 추가
     LocalDateTime start;
     LocalDateTime end;
 }
